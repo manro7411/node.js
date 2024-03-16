41599914206b257781b2baa585116ef8ce963adc
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-16 23:40:56 +0200

    second commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const amount = 12
+if (amount < 10) {
+    console.log('small number')
+}else{
+    console.log('large number')
+}
+
+console.log(`it's my first application!`)
+
+// global - no window 
+// __dirname - path to current directory
+// require - function to use modules 
+// module = info about current module 
+// process - info about env where the program is being executed
